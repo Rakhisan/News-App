3b0fe25f83db8ed6b872b7f104d6302fca3f41b5
Rakhi Singh <rakhisingh030118@gmail.com>
2024-04-21 09:18:24 +0530

    Initial Commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -18,11 +18,7 @@ const NavBar = (props) => {
                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item"><a className="nav-link" href="/business">Business</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/entertainment">Entertainment</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/general">General</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/health">Health</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/science">Science</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/sports">Sports</a></li>
+                            
                             <li className="nav-item"><a className="nav-link" href="/technology">Technology</a></li>
                         </ul>
                         <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
